182c82b0b57aef7d2bb9381fc6cbcc02f4c6fd90
Kai Sato <kairobbiesato@gmail.com>
2021-03-31 17:24:17 +1000

    adding a print statement to testfile

--- Assets/testfile.cs
+++ Assets/testfile.cs
@@ -7,7 +7,7 @@ public class testfile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("testing print");
     }
 
     // Update is called once per frame
