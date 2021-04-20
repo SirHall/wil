0eba606cb5c8870c97f02340f48d3ad0b6eddcbb
SirHall <sirhallstein@gmail.com>
2021-04-20 14:49:53 +1000

    Setup GameManager and allow for scene resetting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] InputAction resetInput;
+
+    void OnEnable()
+    {
+        resetInput.Enable();
+    }
+
+    void Update()
+    {
+        if (resetInput.ReadValue<float>() > 0.5f)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reset game
+    }
+
+    void OnDisable()
+    {
+        resetInput.Disable();
+    }
+}
