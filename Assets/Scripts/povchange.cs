3f21ca112a883770a3e22a87fef3e30727b2486b
Kai <kairobbiesato@gmail.com>
2021-05-11 17:29:46 +1000

    Fixed a bug with povchange

--- Assets/Scripts/povchange.cs
+++ Assets/Scripts/povchange.cs
@@ -9,6 +9,8 @@ public class povchange : MonoBehaviour
     public GameObject gameObj1;
     public GameObject gameObj2;
 
+    bool detected = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,19 +20,24 @@ public class povchange : MonoBehaviour
         SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
         foreach (var xrDisplay in xrDisplaySubsystems)
         {
-            if (xrDisplay.running)
-            {
-                Debug.Log("Detected VR");
-                gameObj1.SetActive(false);
-                gameObj2.SetActive(true);
-
-
-
+            if (xrDisplay.running){
+                detected = true;
             }
         }
-        Debug.Log("Not detected VR");
-        gameObj1.SetActive(true);
-        gameObj2.SetActive(false);
+
+        if (detected)
+        {
+            Debug.Log("Detected VR");
+            gameObj1.SetActive(false);
+            gameObj2.SetActive(true);
+            
+        }
+        else {
+            Debug.Log("Not detected VR");
+            gameObj1.SetActive(true);
+            gameObj2.SetActive(false);
+        }
+        
     }
 
     // Update is called once per frame
