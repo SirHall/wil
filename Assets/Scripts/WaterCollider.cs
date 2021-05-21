de9e8562a684d07b1126ff8facea0ecc4a05e3ee
SirHall <sirhallstein@gmail.com>
2021-05-21 22:57:19 +1000

    Mark WaterCollider as obsolete

--- Assets/Scripts/WaterCollider.cs
+++ Assets/Scripts/WaterCollider.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Obsolete]
 public class WaterCollider : MonoBehaviour
 {
     [SerializeField] Collider sourceCollider;
