e3714e0f62688ad1813d80789ae59be5b7229823
SirHall <sirhallstein@gmail.com>
2021-05-21 22:08:42 +1000

    Setup the WaveSettingEvent GlobalEvent, to annnounce new wave data information

---
+++ Assets/Scripts/GlobalEvents/WaveSettingEvent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSettingEvent : GlobalEvent<WaveSettingEvent>, System.IDisposable
+{
+    public WaveSettings settings;
+}
+
+public struct WaveSettings
+{
+    public float radius;
+    public float length;
+    public float arc;
+    public float squash;
+}
