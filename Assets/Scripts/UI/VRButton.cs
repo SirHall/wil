2fe24b4851943a1651640473e4f3d4538afd2a92
SirHall <sirhallstein@gmail.com>
2021-07-24 16:21:30 +1000

    Add fields to allow buttons to set their color/text, etc

--- Assets/Scripts/UI/VRButton.cs
+++ Assets/Scripts/UI/VRButton.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using TMPro;
 
 public class VRButton : MonoBehaviour
 {
+
+    #region Button Info
+
     [Tooltip("The distance the button must be moved from its starting position by the hand to be considered 'pressed'")]
+    [TabGroup("Settings")]
     [SerializeField] float depressDist = 0.01f;
 
     [Tooltip("If enabled, the button will move back to its original position after being depressed")]
+    [TabGroup("Settings")]
     [SerializeField] bool momentarySwitch = true;
 
+    [TabGroup("Settings")]
     [SerializeField] VRButtons button = VRButtons.None;
 
     [Tooltip("How quickly the button returns to it's original position after being fully depressed")]
+    [TabGroup("Settings")]
     [SerializeField] float buttonRiseLerpSpeed = 1.0f;
 
     [Tooltip("When pressed by a non-physical source (user clicks on this button using their mouse), how far does the butto depress")]
+    [TabGroup("Settings")]
     [SerializeField] float clickDist = 1.0f;
+
+    [Tooltip("What is displayed on the button as text")]
+    [TabGroup("Settings")]
+    [SerializeField] string label = "Press";
+
+    [Tooltip("The color of the button in play")]
+    [TabGroup("Settings")]
+    [SerializeField] Color buttonColor = Color.black;
+
+    #endregion
+
+    [TabGroup("References")]
+    [SerializeField] MeshRenderer meshRend;
+
+    [TabGroup("References")]
+    [SerializeField] TextMeshPro text;
 
     #region Bookkeeping
 
@@ -29,6 +55,9 @@ public class VRButton : MonoBehaviour
     void Start()
     {
         initPos = transform.position;
+
+        text.text = label;
+        meshRend.material.color = buttonColor;
     }
 
     void Update()
