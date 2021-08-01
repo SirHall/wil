0c7a5aecec7354a5232b1ee6894d8a1a4ffd131d
SirHall <sirhallstein@gmail.com>
2021-08-01 17:02:08 +1000

    Refactor DesignerUI to utilize new 3D VR-compatible buttons

--- Assets/Scripts/UI/DesignerUI.cs
+++ Assets/Scripts/UI/DesignerUI.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Sirenix.OdinInspector;
 
 public class DesignerUI : MonoBehaviour
 {
-    [SerializeField] Wave wave;
 
-    [SerializeField] Slider barrelRadiusSlider;
-    [SerializeField] Slider barrelArcSlider;
-    [SerializeField] Slider barrelLengthSlider;
-    [SerializeField] Slider barrelSquashSlider;
+    [Tooltip("The length of the barrel when surfing")]
+    [SerializeField] [TabGroup("Settings")] float barrelLength = 50.0f;
+    [Tooltip("How much to change the barrel length by within the editor")]
+    [SerializeField] [TabGroup("Settings")] float barrelLengthDelta = 5.0f;
+    [Tooltip("The minimum and maximum length of the barrel")]
+    [SerializeField] [TabGroup("Settings")] [MinMaxSlider(0.0f, 500.0f, true)] Vector2 barrelLengthLimits = new Vector2(5.0f, 150.0f);
 
-    [SerializeField] Button surfButton;
+    [Space]
+    [Tooltip("The radius of the barrel reached during the surfing intro animation")]
+    [SerializeField] [TabGroup("Settings")] float barrelRadius = 4.0f;
+    [Tooltip("How much to change the barrel radius by within the editor")]
+    [SerializeField] [TabGroup("Settings")] float barrelRadiusDelta = 0.5f;
+    [Tooltip("The minimum and maximum radius of the barrel")]
+    [SerializeField] [TabGroup("Settings")] [MinMaxSlider(0.0f, 50.0f, true)] Vector2 barrelRadiusLimits = new Vector2(1.0f, 8.0f);
+
+
+    [SerializeField] [TabGroup("References")] Wave wave;
+    [SerializeField] [TabGroup("References")] string surfScene = "Game";
+    [SerializeField] [TabGroup("References")] string mainMenuScene = "Main Menu";
 
     #region Bookkeeping
 
@@ -21,49 +34,54 @@ public class DesignerUI : MonoBehaviour
 
     #endregion
 
+
+
     void Start()
     {
         Time.timeScale = 1.0f;
 
-        //  Firstly get the values from the settings, this allows us to use the
-        // settings used from previous designer scene instances and allows for
-        // apreviously run barrel to be slightly tweaked.
-        barrelRadiusSlider.value = BarrelSettings.Instance.Radius;
-        barrelArcSlider.value = BarrelSettings.Instance.Arc;
-        barrelLengthSlider.value = BarrelSettings.Instance.Length;
-
-        barrelRadiusSlider.onValueChanged.AddListener(UpdateBarrelRadius);
-        barrelArcSlider.onValueChanged.AddListener(UpdateBarrelArc);
-        barrelLengthSlider.onValueChanged.AddListener(UpdateBarrelLength);
-        // barrelSquashSlider.onValueChanged.AddListener(v => wave.BarrelLength = v);
-
-        UpdateBarrelRadius(barrelRadiusSlider.value);
-        UpdateBarrelArc(barrelArcSlider.value);
-        UpdateBarrelLength(barrelLengthSlider.value);
+        UpdateBarrelLength(barrelLength);
+        UpdateBarrelRadius(barrelRadius);
+        UpdateBarrelArc(0.8f);
     }
 
-    void UpdateBarrelRadius(float v) { wave.BarrelRadius = v; BarrelSettings.Instance.Radius = v; }
+    void UpdateBarrelLength(float v)
+    {
+        barrelLength = v.Clamp(barrelLengthLimits);
+        wave.BarrelLength = barrelLength;
+        BarrelSettings.Instance.Length = barrelLength;
+    }
+
+    void UpdateBarrelRadius(float v)
+    {
+        barrelRadius = v.Clamp(barrelRadiusLimits);
+        wave.BarrelRadius = barrelRadius;
+        BarrelSettings.Instance.Radius = barrelRadius;
+    }
+
     void UpdateBarrelArc(float v) { wave.BarrelArc = v; BarrelSettings.Instance.Arc = v; }
-    void UpdateBarrelLength(float v) { wave.BarrelLength = v; BarrelSettings.Instance.Length = v; }
 
 
+    void OnEnable() => VRButtonEvent.RegisterListener(OnVRButtonEvent);
+    void OnDisable() => VRButtonEvent.UnregisterListener(OnVRButtonEvent);
 
-    public void OnSurfButtonClicked()
+
+    void OnVRButtonEvent(VRButtonEvent e)
     {
-        if (!clickedSurf)
+        switch (e.button)
         {
-            clickedSurf = true;
-            StartCoroutine(LoadSim());
-        }
-    }
+            case VRButtons.Surf: SceneManager.LoadScene(surfScene, LoadSceneMode.Single); break;
+            case VRButtons.Exit: SceneManager.LoadScene(mainMenuScene, LoadSceneMode.Single); break;
 
-    IEnumerator LoadSim()
-    {
-        AsyncOperation load = SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
-        load.allowSceneActivation = true;
-        while (!load.isDone)
-            yield return null;
-        SceneManager.UnloadSceneAsync("BarrelDesigner");
+            case VRButtons.Designer_IncreaseRadius: UpdateBarrelRadius(barrelRadius + barrelRadiusDelta); break;
+            case VRButtons.Designer_DecreaseRadius: UpdateBarrelRadius(barrelRadius - barrelRadiusDelta); break;
+
+            case VRButtons.Designer_Longer: UpdateBarrelLength(barrelLength + barrelLengthDelta); break;
+            case VRButtons.Designer_Shorter: UpdateBarrelLength(barrelLength - barrelLengthDelta); break;
+
+            default:
+                break; // Ignore all other buttons
+        }
     }
 
 }
