9659dc27d699ea3f45b48224fba5c97f4dbcf8aa
SirHall <sirhallstein@gmail.com>
2021-08-01 16:56:30 +1000

    Work on Input KeyboardMode scripts to specially handle keyboard code

---
+++ Assets/Scripts/Input/KeyboardMode/Keyboard_RotateHead.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.InputSystem;
+
+public class Keyboard_RotateHead : MonoBehaviour
+{
+
+
+    void Update()
+    {
+
+    }
+}
