c1daf211bb7537a57ff94c8fc00c439c4fff1e11
Jfarr <jakobpaul13@gmail.com>
2021-08-08 15:39:27 +1000

    Add - HeadHeight script to find the heads Y coordinates for the current active camera (either keyboard or vr camera). Add - LeaningEffect method within VisualEffects and moved leaning code into it. Add - UnderwaterEffects method within Visual effects which toggles the underwater post processing when the player is below the water level Add - HeadHeight script to Character prefab Add - Underwater post processing effects into Post Processing Prefab.

--- Assets/Scripts/VisualEffects.cs
+++ Assets/Scripts/VisualEffects.cs
@@ -12,7 +12,8 @@ public class VisualEffects : MonoBehaviour
 
     private Volume postProcessingVolume;
 
-    Vector3 dir = new Vector3();
+    // Players head position
+    Vector3 headPos = new Vector3();
 
     void OnEnable()
     {
@@ -27,7 +28,7 @@ public class VisualEffects : MonoBehaviour
     // A controller has announced new data
     void OnMoveControlEvent(VisualControlEvent e)
     {
-        dir = e.dir;
+        headPos = e.dir;
     }
 
     // Start is called before the first frame update
@@ -39,12 +40,36 @@ public class VisualEffects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        LeaningEffect();
+        UnderwaterEffect();
+    }
+    /// <summary>
+    /// Enables the leaning effects when the players head is detected leaning from the center of the board
+    /// </summary>
+    private void LeaningEffect()
+    {
         if (postProcessingVolume.profile.TryGet<Vignette>(out var vignette))
         {
-            float headPosDist = Mathf.Max(Mathf.Abs(dir.z), Mathf.Abs(dir.x));
+            float headPosDist = Mathf.Max(Mathf.Abs(headPos.z), Mathf.Abs(headPos.x));
 
             vignette.intensity.overrideState = true;
             vignette.intensity.value = headPosDist + 0.1f;
         }
+    }
+    /// <summary>
+    /// Enables the underwater effects when the player is below the water level
+    /// </summary>
+    private void UnderwaterEffect()
+    {
+        bool isUnderWater = false;
+        // Check if players position is below the water
+        if (headPos.y <= 0) isUnderWater = true; 
+        else isUnderWater = false;
+
+        //Enable or disable all existing effects on post processing game object which create an underwater effect.
+        if (postProcessingVolume.profile.TryGet<DepthOfField>(out var dof)) dof.active = isUnderWater;
+        if (postProcessingVolume.profile.TryGet<ColorAdjustments>(out var colorAdjust)) colorAdjust.active = isUnderWater;
+        if (postProcessingVolume.profile.TryGet<WhiteBalance>(out var whiteBalance)) whiteBalance.active = isUnderWater;
+        if (postProcessingVolume.profile.TryGet<ShadowsMidtonesHighlights>(out var shadows)) shadows.active = isUnderWater;
     }
 }
