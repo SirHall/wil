c2399104780fde57ea14016f57825e38c67135e6
Jfarr <jakobpaul13@gmail.com>
2021-08-16 21:03:29 +1000

    Modified - Coral See distance from 30 to 10 Add - Condition to check if coral is within camera FOV

--- Assets/Scripts/CoralCulling.cs
+++ Assets/Scripts/CoralCulling.cs
@@ -20,6 +20,7 @@ public class CoralCulling : MonoBehaviour
         // Add all coral within parent gameobject to coralList
         foreach (Transform child in transform)
             coralList.Add(child.gameObject);
+
     }
 
     void LateUpdate()
@@ -29,10 +30,13 @@ public class CoralCulling : MonoBehaviour
         {
             Transform coralChild = coral.transform.GetChild(0);
             Vector3 closestToPlayer = coralChild.gameObject.GetComponent<MeshRenderer>().bounds.ClosestPoint(player.transform.position);
+            Vector3 directionToObject = closestToPlayer - player.transform.position;
 
-            bool visible = Vector3.Distance(closestToPlayer, player.transform.position) <= seeDist;
+            bool visible = Vector3.Distance(closestToPlayer, player.transform.position) <= seeDist && Vector3.Angle(Camera.main.transform.forward, directionToObject) < Camera.main.fieldOfView + 20f;
+
             if (coral.activeSelf != visible) // Only update active state if it needs to be changes
                 coral.gameObject.SetActive(visible);
+            
 
         }
     }
