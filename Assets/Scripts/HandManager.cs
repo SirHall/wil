0237e97a006be5116ee0ec2fd044503da28788b4
Jfarr <jakobpaul13@gmail.com>
2021-08-26 19:07:32 +1000

    Add - Grabbing Boundary Reference in surfboard prefab Modified - Left and Right HandManagers to use Held In Hand characteristic from Controller Modified - StartCoordinate variable name within HeadMovement script to fix spelling error Modified - HandManager to include methods to scale and return hand position relative to 0 and 1 (max value). Modified - HandManager to include method to scale relative hand position to board input (WIP).

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -20,7 +20,21 @@ public class HandManager : MonoBehaviour
 
     private Animator handAnimator;
 
+    private bool isInteracting = false;
+
     Interactables interaction = new Interactables();
+
+    [Tooltip("The max unit values the player can move in specified direction")]
+    public float maxUp, maxDown;
+
+    [SerializeField]
+    [Tooltip("Starting local Cooridnate of the player")]
+    private Vector3 startCoordinate;
+
+    /// <summary>
+    /// The player's hand position relative to the starting hand position
+    /// </summary>
+    private Vector3 handPosRel = Vector3.zero;
 
     void OnEnable() {
         InteractablesEvent.RegisterListener(OnGripControlEvent);
@@ -32,6 +46,12 @@ public class HandManager : MonoBehaviour
     // A controller has announced new data
     void OnGripControlEvent(InteractablesEvent e) {
         interaction = e.interactables;
+    }
+
+    private void CallGlobalEvents()
+    {
+        using (var e = BoardControlEvent.Get())
+            e.input.dir = HandPosToBoardInput(handPosRel);
     }
 
     // Start is called before the first frame update
@@ -61,7 +81,32 @@ public class HandManager : MonoBehaviour
 
         // Run Animations
         HandAnimation();
+
+        CallGlobalEvents();
+        CheckHandPosition();
         InteractableGripping();
+    }
+
+    private Vector3 HandPosToBoardInput(Vector3 handPos)
+    {
+        Vector3 dir = Vector3.zero;
+
+        // Apply forward movement if the player leans 40% or greater based on the max forward value.
+        // and use 30% on the sides
+
+        // Cutoff values determin when the head position values should be ignored and returned as a 0 value (Essentially making the surfboard stationary)
+        // Or when they should return their true values which will be used by the surfboard to move it. 
+        float heightCutoff = 0.2f;
+
+        // Moving
+        //if (handPos.y >= heightCutoff) { dir.y = handPos.x - heightCutoff; } // Height
+        if (handPos.y >= heightCutoff) { dir.x = -handPos.z + heightCutoff; } //Left
+        if (handPos.y <= -heightCutoff) { dir.x = Mathf.Abs(handPos.z + heightCutoff); } //Right
+
+        // Stationary
+        if (handPos.y < heightCutoff && handPos.y > -heightCutoff) { dir.x = 0; } // Side
+
+        return dir;
     }
 
     /// <summary>
@@ -115,12 +160,48 @@ public class HandManager : MonoBehaviour
     /// </summary>
     private void InteractableGripping() 
     {
+        bool previousInteractionState = isInteracting;
         if (devices[0].TryGetFeatureValue(CommonUsages.grip, out float gripvalue)) 
         {
-            if (gripvalue == 1 && interaction == Interactables.Surfboard) 
-            {
-                print("Player is gripping side of board");
-            }
+            if (gripvalue == 1 && interaction == Interactables.Surfboard) isInteracting = true;
+            else isInteracting = false;
+            
         }
+
+        if (isInteracting)
+        {
+            if (previousInteractionState != isInteracting) startCoordinate = visibleHandModel.transform.localPosition;
+
+        }
+        
+        
+    }
+
+    /// <summary>
+    /// Return positive value that is scaled between 0 and a given max value
+    /// </summary>
+    /// <param name="value">Difference value based off current position - Starting position</param>
+    /// <param name="maxValue">Max difference value for given direction</param>
+    /// <returns>Positive float scaled between 0 and given max value</returns>
+    private float DirectionScale(float value, float maxValue)
+    {
+        // Clamp and scale positive value based off given maxValue
+        return Mathf.Clamp(Mathf.Abs(value), 0, maxValue) / maxValue;
+    }
+
+    /// <summary>
+    /// Manages head position values based on current coordinates from start coordinates
+    /// </summary>
+    void CheckHandPosition()
+    {
+        // Diff X axis = Forward
+        // Diff Z Axis = Side
+        Vector3 diff = visibleHandModel.transform.localPosition - startCoordinate;
+
+        // Assign variable values based on the direction the player is leaning / standing
+        if (diff.y > 0)
+            handPosRel.y = DirectionScale(diff.y, maxUp); // Positive
+        else
+            handPosRel.y = -DirectionScale(diff.y, maxDown); // Negative
     }
 }
