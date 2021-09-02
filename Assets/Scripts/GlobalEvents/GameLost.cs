579ddcd75115bd8e04482af4d2fa694490d4ca81
SirHall <sirhallstein@gmail.com>
2021-09-02 13:10:17 +1000

    Add 'cause' field to GameLost global event to denote the cause of a fall

--- Assets/Scripts/GlobalEvents/GameLost.cs
+++ Assets/Scripts/GlobalEvents/GameLost.cs
@@ -2,4 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameLost : GlobalEvent<GameLost>, System.IDisposable { }
+public class GameLost : GlobalEvent<GameLost>, System.IDisposable
+{
+    public string cause = "";
+}
