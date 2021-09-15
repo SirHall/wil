ccdca7d94348c4bfd1b69261e003588ffb55750c
SirHall <sirhallstein@gmail.com>
2021-09-15 13:01:34 +1000

    Refactor keyboard/vr availability scripts to utilize InputAvailability rather than just destroying themselves so as to allow for realtime input switching support

--- Assets/InputDefinitions/Keyboard/KeyboardAvailability.cs
+++ Assets/InputDefinitions/Keyboard/KeyboardAvailability.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyboardAvailability : MonoBehaviour
+public class KeyboardAvailability : InputAvailability
 {
+    protected override void Start() => base.Start();
+
     // Nothing really needs to happen here, we assume that the keyboard is always available
     // TODO: Might want to do something here to support consoles and other non-PC platforms
-    void Start()
-    {
-
-    }
+    protected override bool CheckAvailability() => true;
 }
--- Assets/InputDefinitions/VR/VRAvailability.cs
+++ Assets/InputDefinitions/VR/VRAvailability.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class VRAvailability : MonoBehaviour
+public class VRAvailability : InputAvailability
 {
     private List<InputDevice> headsetDevices = new List<InputDevice>();
 
-    void Start()
+    protected override void Start() => base.Start();
+
+    protected override bool CheckAvailability()
     {
         InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeadMounted, headsetDevices);
-        // There are no VR headsets
-        if (headsetDevices.Count == 0)
-            Destroy(gameObject);
+        // There are VR headsets if we have atleast one connected
+        return headsetDevices.Count > 0;
     }
 }
